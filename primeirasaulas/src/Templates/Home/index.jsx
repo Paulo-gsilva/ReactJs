1413bf0a0968b33c0b97f70e85e9062a720223c9
Paulo-gsilva <guigui041201@gmail.com>
2022-04-06 18:44:09 -0300

    convertendo state para hooks

--- primeirasaulas/src/Templates/Home/index.jsx
+++ primeirasaulas/src/Templates/Home/index.jsx
@@ -1,138 +1,73 @@
 import './style.css';
-import { Component } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import LoadPosts from '../../Func/load.posts';
 import Posts from '../../Components/Posts';
 import Button from '../../Components/Button';
 import SearchInput from '../../Components/TextInput';
 
-class Home extends Component {
-  state = {
-    posts: [],
-    allPosts: [],
-    page: 0,
-    postsPerPage: 2,
-    searchValue: ''
-  };
+export default function Home() {
+  const [posts, setPosts] = useState([]);
+  const [allPosts, setAllPosts] = useState([]);
+  const [page, setPage] = useState(0);
+  const [postsPerPage] = useState(4);
+  const [searchValue, setSearchValue] = useState('');
 
-  componentDidMount() {
-    this.loadPosts();
-  }
+  const filteredPosts = !!searchValue ?
+    posts.filter(post => {
+      return post.title.toLocaleLowerCase().includes(searchValue.toLocaleLowerCase());
+    })
+    : posts;
 
-  loadPosts = async () => {
-    const { page, postsPerPage } = this.state;
+  const handleLoadPosts = useCallback(async (page, postsPerPage) => {
 
     const postsAndPhotos = await LoadPosts();
-    this.setState({
-      posts: postsAndPhotos.slice(page, postsPerPage),
-      allPosts: postsAndPhotos
-    });
+
+    setPosts(postsAndPhotos.slice(page, postsPerPage));
+    setAllPosts(postsAndPhotos);
+  }, []);
+
+  useEffect(() => {
+    handleLoadPosts(0, postsPerPage);
+  }, [handleLoadPosts, postsPerPage])
+
+  const handleChange = (event) => {
+    const { value } = event.target;
+
+    setSearchValue(value);
   }
 
-  loadMorePosts = () => {
-    const { page, postsPerPage, posts, allPosts } = this.state;
-
+  const loadMorePosts = () => {
     const nextPage = page + postsPerPage;
     const nextPosts = allPosts.slice(nextPage, nextPage + postsPerPage);
 
     posts.push(...nextPosts);
 
-    this.setState({ posts, page: nextPage });
+    setPosts(posts);
+    setPage(nextPage);
   }
 
-  handleChange = (event) => {
-    const { value } = event.target;
-    this.setState({ searchValue: value });
-  }
-
-  render() { //quando ocorre alguma alteração na página ela é renderizada
-    const { posts, searchValue } = this.state;
-    const filteredPosts = !!searchValue ?
-      posts.filter(post => {
-        return post.title.toLocaleLowerCase().includes(searchValue.toLocaleLowerCase());
-      })
-      : posts;
-
-    return (
-      <>
-        <div className='search'>
-          {!!searchValue && ( //se searchvalue for true (não vazio) mostrará o h1
-            <h1>Search Value</h1>
-          )}
-          <SearchInput
-            handleChange={this.handleChange}
-            value={searchValue}
-          />
-        </div>
-
-        {filteredPosts.length > 0 && (
-          <Posts posts={filteredPosts} />
+  return (
+    <>
+      <div className='search'>
+        {!!searchValue && ( //se searchvalue for true (não vazio) mostrará o h1
+          <h1>Search Value</h1>
         )}
-        {filteredPosts.length === 0 && (
-          <p>Não Existem Posts :(</p>
-        )}
-
-        {!searchValue && ( //se searchvalue for false (vazio) ocultará o botão
-          <Button onClick={this.loadMorePosts} />
-        )}
-      </>
-    );
-  }
-
-}
-export default Home;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-  state = {
-    name: 'Paulo',
-    age: 20
-  };
-
-  handleH1Click = () => {
-    this.setState({ name: 'Guilherme', age: 18 }); //ocorre a alteração do estado
-  }
-
-  render() { //quando ocorre alguma alteração na página ela é renderizada
-    const { name, age } = this.state;
-
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 onClick={this.handleH1Click}>Nome: {name}, Idade: {age}</h1>
-        </header>
+        <SearchInput
+          handleChange={handleChange}
+          value={searchValue}
+        />
       </div>
-    );
-  }
-  */
 
-/*
-componentDidMount() { //ATIVADO QUANDO UM COMPONENTE TERMINA DE SER MONTADO
-  this.handleTimeOut();
-  console.log('mount'); //CONSOLE MOSTRA QUE O DIDMOUNT JÁ FOI CHAMADO, PORÉM CONTINUA ESPERANDO REQUISIÇÕES
+      {filteredPosts.length > 0 && (
+        <Posts posts={filteredPosts} />
+      )}
+      {filteredPosts.length === 0 && (
+        <p>Não Existem Posts :(</p>
+      )}
+
+      {!searchValue && ( //se searchvalue for false (vazio) ocultará o botão
+        <Button onClick={loadMorePosts} />
+      )}
+    </>
+  );
 }
-
-componentDidUpdate() {
-  console.log('update');
-}
-
-handleTimeOut = () => {
-  const { posts } = this.state;
-  posts[0].title = 'Título Alterado';
-
-  setTimeout(() => {
-    this.setState({ posts });
-  }, 3000)
-}*/
