85280488271eb1c872a63ac111bc6592a75a1627
Paulo Gonçalves <guigui041201@gmail.com>
2022-12-10 18:21:52 -0300

    Criando arquivos para CRUD Travel

---
+++ Travel/scripts/creatatabletravelsystem.sql
@@ -0,0 +1,8 @@
+use travelsystem;
+create table TouristSpot(
+	TouristSpotId BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    TouristSpotName VARCHAR(200) NOT NULL,
+    TouristSpotCountry VARCHAR(200)NOT NULL,
+    TouristSpotImage VARCHAR(200)NOT NULL,
+    TouristSpotAddData DATE NOT NULL
+);
