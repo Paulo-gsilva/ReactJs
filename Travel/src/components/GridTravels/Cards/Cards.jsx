5e1854cb5c5395cdbf548b97fc7d34ce25d2e961
Paulo Gonçalves <guigui041201@gmail.com>
2022-12-12 10:16:58 -0300

    Receiving data from the api and sending it to the front

--- Travel/src/components/GridTravels/Cards/Cards.jsx
+++ Travel/src/components/GridTravels/Cards/Cards.jsx
@@ -1,18 +1,29 @@
 import React from "react";
 import "../../../styles/gridtravels.css";
 
-function Cards() {
+function Cards({ travels }) {
   return (
-    <div className="cards-travel">
-      <div className="card-img">
-        <h1>image</h1>
-      </div>
-      <div className="card-text">
-        <h3>Destino</h3>
-        <h4>País - Cidade</h4>
-        <p>Creation date</p>
-      </div>
-    </div>
+    <>
+      {travels.map((travel, index) => {
+        return (
+          <div key={travel.TouristSpotId} className="cards-travel">
+            <div className="card-img">
+              <img
+                src={travel.TouristSpotImage}
+                alt={`Imagem de ${travel.TouristSpotName}`}
+              />
+            </div>
+            <div className="card-text">
+              <h3>{travel.TouristSpotName}</h3>
+              <h4>
+                {travel.TouristSpotCountry} - {travel.TouristSpotCity}
+              </h4>
+              <p>Adicionado em: {travel.TouristSpotAddData}</p>
+            </div>
+          </div>
+        );
+      })}
+    </>
   );
 }
 
--- Travel/src/components/GridTravels/GridTravels.jsx
+++ Travel/src/components/GridTravels/GridTravels.jsx
@@ -1,16 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import Cards from "./Cards/Cards";
 import "../../styles/gridtravels.css";
+import { toast } from "react-toastify";
 
 function GridTravels() {
+  const [travels, setTravels] = useState([]);
+
+  const getTravelList = async () => {
+    try {
+      const travelsList = await axios.get("http://localhost:8080");
+      setTravels(travelsList.data);
+    } catch (error) {
+      console.log(error);
+      toast.error("Não foi possível adicionar uma nova viagem 😥");
+    }
+  };
+
+  useEffect(() => {
+    getTravelList();
+  }, [setTravels]);
+
   return (
     <section className="section-gridtravels">
-      <Cards />
-      <Cards />
-      <Cards />
-      <Cards />
-      <Cards />
-      <Cards />
+      <Cards travels={travels} />
     </section>
   );
 }
